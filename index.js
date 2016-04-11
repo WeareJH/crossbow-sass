d6140587c28b713a06be665f168af66bffe44b5e
JodiWarren <jodi@jodiwarren.com>
2016-04-11 13:56:00 +0100

    Handle sass errors
    Prevents crossbow from exiting a watch process when it runs into a Sass compilation error.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var imp        = require('postcss-import');
 function processSass (obs, opts, ctx) {
     return ctx.vfs.src(opts.input)
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(post([imp, pre, cssnano]))
         .pipe(sourcemaps.write('.'))
         .pipe(ctx.vfs.dest(opts.output));
